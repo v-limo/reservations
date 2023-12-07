97e9699cc476f4a5d9adb32b96fa2bb7d36eb5bd
Vincent Limo <limovincenti@gmail.com>
2023-12-07 15:33:58 +0200

    migrations fourth

---
+++ Reservations.Api/Migrations/20231207133143_forthmigrations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reservations.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class forthmigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
