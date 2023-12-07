1fae9971b3db33249b1beccb98060a02bf600c5c
Vincent Limo <limovincenti@gmail.com>
2023-12-07 15:07:10 +0200

    fix: sql server to postgres

--- Reservations.Api/Program.cs
+++ Reservations.Api/Program.cs
@@ -3,19 +3,27 @@ using Reservations.Api.Data;
 using Reservations.Api.Profiles;
 using Reservations.Api.Services.Implementation;
 using Reservations.Api.Services.Interfaces;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
-string? connectionString;
-connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var postgresCoonection = builder.Configuration.GetConnectionString("PostgresConnection");
 
 
 
 // Add services to the container.
+// builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(postgresCoonection));
+
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
 builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
